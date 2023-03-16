b361a3a33e7b58e37a5dc3e5f4ffb1207230874b
Colin Moriarity <cm311@student.london.ac.uk>
2023-03-16 17:35:38 -0400

    fixed indentation in text

--- app_site_main/admin.py
+++ app_site_main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db import models
 from .models import Post, Tag, WH3Lord
 from .forms import PostForm
 
@@ -10,9 +12,14 @@ class PostAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
 
+class WH3LordAdmin(admin.ModelAdmin):
+    formfield_overrides =  {
+        models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 80, 'style': 'font-family: monospace; white-space: pre;'})},
+    }
+
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Tag)
-admin.site.register(WH3Lord)
+admin.site.register(WH3Lord, WH3LordAdmin)
 
